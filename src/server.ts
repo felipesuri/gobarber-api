cc78db789f22b70f47d0daae9d9ef0ecc3a3e9ec
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-22 11:05:51 -0300

    :recycle: modified code style

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' })
+})
 
-const PORT = 3333;
+const PORT = 3333
 
 app.listen(PORT, () => {
-  console.log(`> Server started on port: ${PORT} `);
-});
+  console.log(`> Server started on port: ${PORT}`)
+})
