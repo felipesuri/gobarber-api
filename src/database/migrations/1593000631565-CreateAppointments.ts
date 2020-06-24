900b53384f8098f4a71a8bc20f7308bbbe3f5d62
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-24 10:44:33 -0300

    :card_file_box: modified appointments

--- src/database/migrations/1593000631565-CreateAppointments.ts
+++ src/database/migrations/1593000631565-CreateAppointments.ts
@@ -22,6 +22,16 @@ export default class CreateAppointments1593000631565 implements MigrationInterfa
             name: 'date',
             type: 'timestamp with time zone',
             isNullable: false
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
           }
         ]
       })
