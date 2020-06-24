f09cc937afd551b5d2e042af0a6988841195e7cd
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-24 11:15:37 -0300

    :card_file_box: modified migrations

--- src/database/migrations/1593005140229-CreateUser.ts
+++ src/database/migrations/1593005140229-CreateUser.ts
@@ -8,7 +8,7 @@ export default class CreateUser1593005140229 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
