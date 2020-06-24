d81fa80f8cb0e54db20b1cc65a0e0e0002025ecd
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-24 19:09:50 -0300

    :zap: implemented JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,8 @@
 import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
+import authConfig from '../config/auth'
 
 import User from '../models/User'
 
@@ -10,6 +13,7 @@ interface Request {
 
 interface Response {
   user: User
+  token: string
 }
 
 class AuthenticateUserService {
@@ -28,7 +32,14 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination')
     }
 
-    return { user }
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn
+    })
+
+    return { user, token }
   }
 }
 
