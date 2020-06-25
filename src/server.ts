ab91d810c1abb3e00fc1b1a93575a231a14a727a
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-25 06:52:03 -0300

    :truck: add view for static files

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata'
 
 import express from 'express'
 
+import uploadConfig from './config/upload'
+
 import './database'
 
 import routes from './routes'
@@ -9,6 +11,7 @@ import routes from './routes'
 const app = express()
 
 app.use(express.json())
+app.use('/files', express.static(uploadConfig.directory))
 app.use(routes)
 
 const PORT = 3333
