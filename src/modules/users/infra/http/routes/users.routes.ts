80a8e8fbba74caf69a76cea892ae3719853968d6
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-14 08:18:36 -0300

    :truck: modified routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -8,6 +8,8 @@ import ensureAuthenticated from '@modules/users/infra/middleware/ensureAuthentic
 import CreateUserService from '@modules/users/services/CreateUserServive'
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository'
+
 const usersRouter = Router()
 const upload = multer(uploadConfig)
 
@@ -15,7 +17,8 @@ usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body
 
-    const createUser = new CreateUserService()
+    const usersRepository = new UsersRepository()
+    const createUser = new CreateUserService(usersRepository)
 
     const user = await createUser.execute({
       name,
@@ -35,7 +38,8 @@ usersRouter.patch('/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService()
+    const usersRepository = new UsersRepository()
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository)
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
