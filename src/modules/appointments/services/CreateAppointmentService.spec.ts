9ff87c90b7269c65a42ea914c28840bc04e53c76
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-15 17:48:16 -0300

    :alembic: create test for error on create appointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/appError'
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
 import CreateAppointmentService from './CreateAppointmentService'
 
@@ -15,7 +17,22 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('121212')
   })
 
-  // it('should not be able to create two appointment on the same time', () => {
-  //   expect(1 + 1).toBe(2)
-  // })
+  it('should not be able to create two appointment on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository()
+    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository)
+
+    const appointmentDate = new Date(2020, 6, 10, 11)
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '121212'
+    })
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '121213'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
 })
