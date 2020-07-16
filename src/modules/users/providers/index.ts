f5746e61684cd1907158bcfb90ae59b0a1700002
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-16 01:50:53 -0300

    :label: create container Provider

---
+++ src/modules/users/providers/index.ts
@@ -0,0 +1,9 @@
+import { container } from 'tsyringe'
+
+import iHashProvider from './HashProvider/models/iHashProvider'
+import BCryptProvider from './HashProvider/implementations/BCryptHashProvider'
+
+container.registerSingleton<iHashProvider>(
+  'HashProvider',
+  BCryptProvider
+)
