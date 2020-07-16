1887b0138e81e127fc4fff1e46c4312be0e96e94
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-16 01:53:16 -0300

    :package: implemented FakeHashProvider

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,12 +1,15 @@
 import AppError from '@shared/errors/appError'
 
 import FakeUsersRepository from '../repositories/fakes/fakeUsersRepository'
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashPovider'
 import CreateUserService from './CreateUserService'
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
-    const createUser = new CreateUserService(fakeUsersRepository)
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
 
     const user = await createUser.execute({
       name: 'John Doe',
@@ -19,7 +22,9 @@ describe('CreateUser', () => {
 
   it('should be not able to create a new user with same email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository()
-    const createUser = new CreateUserService(fakeUsersRepository)
+    const fakeHashProvider = new FakeHashProvider()
+
+    const createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider)
 
     await createUser.execute({
       name: 'John Doe',
