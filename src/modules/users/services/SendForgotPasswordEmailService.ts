a19f6a1e39998ea6887340d3c66605bd5638dd24
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-21 17:34:48 -0300

    :truck: create SendForgotPasswordMailService

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,40 @@
+import { injectable, inject } from 'tsyringe'
+
+import AppError from '@shared/errors/appError'
+
+// import User from '../infra/typeorm/entities/User'
+import iUsersRepository from '../repositories/iUsersRepository'
+import iMailProvider from '@shared/container/providers/MailProvider/models/iMailProvider'
+import iUserTokensRepository from '@modules/users/repositories/iUserTokensRepository'
+
+interface Request {
+  email: string
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: iUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: iMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: iUserTokensRepository
+  ) { }
+
+  public async execute({ email }: Request): Promise<void> {
+    const user = await this.usersRepository.findByMail(email)
+
+    if (!user) {
+      throw new AppError('User this not exists.')
+    }
+
+    await this.userTokensRepository.generate(user.id)
+
+    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido')
+  }
+}
+
+export default SendForgotPasswordEmailService
