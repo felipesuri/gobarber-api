d88b0e2b80c63277a98d9aaafdf75304183b5ee6
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-22 18:50:16 -0300

    :sparkles: implemented mail template

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,7 +2,6 @@ import { injectable, inject } from 'tsyringe'
 
 import AppError from '@shared/errors/appError'
 
-// import User from '../infra/typeorm/entities/User'
 import iUsersRepository from '../repositories/iUsersRepository'
 import iMailProvider from '@shared/container/providers/MailProvider/models/iMailProvider'
 import iUserTokensRepository from '@modules/users/repositories/iUserTokensRepository'
@@ -33,10 +32,20 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id)
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recuperação de senha recebido, utilize esse token: ${token}`
-    )
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}, segue seu token: {{token}}',
+        variables: {
+          name: user.name,
+          token
+        }
+      }
+    })
   }
 }
 
