f90db41a867846fa3f526811cc6c74a9480bb701
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-22 08:20:03 -0300

    :package: implemented MailProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe'
 import iStorageProvider from './StorageProvider/models/iStorageProvider'
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
 
+import iMailProvider from './MailProvider/models/iMailProvider'
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider'
+
 container.registerSingleton<iStorageProvider>(
   'StorageProvider',
   DiskStorageProvider
+)
+
+container.registerInstance<iMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider()
 )
