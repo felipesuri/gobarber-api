f87b8e85cbf5814dd83f1a567e2ae8e0df9b3e54
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-23 18:48:35 -0300

    :package: create ShowProfileService

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,30 @@
+import { injectable, inject } from 'tsyringe'
+
+import AppError from '@shared/errors/appError'
+
+import User from '../infra/typeorm/entities/User'
+import iUsersRepository from '../repositories/iUsersRepository'
+
+interface Request {
+  user_id: string
+}
+
+@injectable()
+class ShowProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: iUsersRepository
+  ) { }
+
+  public async execute({ user_id }: Request): Promise<User> {
+    const user = await this.usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new AppError('User not found.')
+    }
+
+    return user
+  }
+}
+
+export default ShowProfileService
