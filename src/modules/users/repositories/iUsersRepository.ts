54a6a6435f03df18ced7a69840d7b114755db864
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-25 05:49:57 -0300

    :package: implemented FindAllProviders

--- src/modules/users/repositories/iUsersRepository.ts
+++ src/modules/users/repositories/iUsersRepository.ts
@@ -1,7 +1,9 @@
 import User from '../infra/typeorm/entities/User'
 import iCreateUserDTO from '../dtos/iCreateUserDTO'
+import iFindAllProvidersDTO from '../dtos/iFindAllProvidersDTO'
 
 export default interface iUsersRepository {
+  findAllProviders(data: iFindAllProvidersDTO): Promise<User[]>
   findById(id: string): Promise<User | undefined>
   findByMail(email: string): Promise<User | undefined>
   create(data: iCreateUserDTO): Promise<User>
