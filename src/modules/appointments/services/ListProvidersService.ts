75274b2b47f2e44f6fdb4997b55f321a6538b51d
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-25 08:14:22 -0300

    :package: create ListProviderService

---
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,26 @@
+import { injectable, inject } from 'tsyringe'
+
+import User from '@modules/users/infra/typeorm/entities/User'
+import iUsersRepository from '@modules/users/repositories/iUsersRepository'
+
+interface Request {
+  user_id: string
+}
+
+@injectable()
+class ListProvidersService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: iUsersRepository
+  ) { }
+
+  public async execute({ user_id }: Request): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id
+    })
+
+    return users
+  }
+}
+
+export default ListProvidersService
