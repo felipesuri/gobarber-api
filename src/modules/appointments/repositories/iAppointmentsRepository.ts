fd824e20447dddad3b3ac3385a25fdf7c2b35653
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-25 11:37:50 -0300

    :package: implemented FindAllInDayFromProvider

--- src/modules/appointments/repositories/iAppointmentsRepository.ts
+++ src/modules/appointments/repositories/iAppointmentsRepository.ts
@@ -1,9 +1,11 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
 import iCreateAppointmentDTO from '../dtos/iCreateAppointmentDTO'
 import iFindAllInMonthFromProviderDTO from '../dtos/iFindAllInMonthFromProviderDTO'
+import iFindAllInDayFromProviderDTO from '../dtos/iFindAllInDayFromProviderDTO'
 
 export default interface iAppointmentsRepository {
   create(data: iCreateAppointmentDTO): Promise<Appointment>
   findByDate(date: Date): Promise<Appointment | undefined>
   findAllInMonthFromProvider(data: iFindAllInMonthFromProviderDTO): Promise<Appointment[]>
+  findAllInDayFromProvider(data: iFindAllInDayFromProviderDTO): Promise<Appointment[]>
 }
