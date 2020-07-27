ac830bca72c07884d8939261307833cdc7ea890b
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-27 10:39:42 -0300

    :rotating_light: configure celebrate

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import ensureAuthenticated from '@modules/users/infra/middleware/ensureAuthenticated'
 
@@ -12,6 +13,16 @@ const providerAppointmentsController = new ProviderAppointmentsController()
 appointmentsRouter.use(ensureAuthenticated)
 
 appointmentsRouter.get('/me', providerAppointmentsController.index)
-appointmentsRouter.post('/', appointmentsController.create)
+
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date().required(),
+    },
+  }),
+  appointmentsController.create
+)
 
 export default appointmentsRouter
