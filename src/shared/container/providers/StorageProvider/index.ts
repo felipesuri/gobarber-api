26f5a8e14d109bd1610e778f74f518c4a43581bc
Felipe Suri <felipeheredia1234@gmail.com>
2020-07-29 10:40:51 -0300

    :sparkles: implemented Amazon S3

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,10 +1,16 @@
 import { container } from 'tsyringe'
+import uploadConfig from '@config/upload'
 
 import iStorageProvider from '../StorageProvider/models/iStorageProvider'
 import DiskStorageProvider from '../StorageProvider/implementations/DiskStorageProvider'
+import S3StorageProvider from '../StorageProvider/implementations/S3StorageProvider'
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 }
 
-container.registerSingleton<iStorageProvider>('StorageProvider', providers.disk)
+container.registerSingleton<iStorageProvider>(
+  'StorageProvider',
+  providers[uploadConfig.driver]
+)
