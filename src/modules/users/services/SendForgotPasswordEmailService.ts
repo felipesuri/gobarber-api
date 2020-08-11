2f88dfec13317e9916b33776a0a5bfda9687d037
Felipe Suri <felipeheredia1234@gmail.com>
2020-08-11 18:09:58 -0300

    :zap: modified link for email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -50,7 +50,7 @@ class SendForgotPasswordEmailService {
         file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          link: `${process.env.WEB_URL}/reset_password/token=${token}`,
+          link: `${process.env.WEB_URL}/reset-password/?token=${token}`,
         },
       },
     })
